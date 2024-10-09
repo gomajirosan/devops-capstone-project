e520c260306a8953d8ccd34775d969c298272dc9
gomajirosan <gomajirosan@gmail.com>
2024-10-09 10:06:05 -0400

    follow PEP8

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
